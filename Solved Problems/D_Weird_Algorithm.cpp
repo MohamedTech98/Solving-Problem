bcd7883805249f9d422245ca4df499f1bcb93d6a
Mohamed Ahmed <mohamedahmed.dev98@gmail.com>
2025-11-29 19:44:32 +0200

    You are ready?

---
+++ Solved Problems/D_Weird_Algorithm.cpp
@@ -0,0 +1,26 @@
+// #include<bits\stdc++.h>
+#include<iostream>
+#include<vector>
+#define ll long long
+#define endl '\n'
+#define ana_Yano ios::sync_with_stdio(false),cin.tie(nullptr)
+using namespace std;
+// vector<int> ans;
+void solve(ll n) {
+    cout<<n<<" ";
+    if(n==1) {
+        return;
+    }
+    if(n%2==0) {
+        solve(n/2);
+    }
+    else {
+        solve(3*n+1);
+    }
+}
+int main() {
+    ana_Yano;
+    int n; cin>>n;
+    solve(n);
+    return 0;
+}
